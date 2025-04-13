684e79b493ce32540dceec65a4a23a749a45bd35
tatiSmol <judeclapton@icloud.com>
2025-04-14 00:03:56 +0300

    time has been set to zero time zone

--- logger.py
+++ logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
-from datetime import datetime
+import time
+from datetime import datetime, timezone
 
 
 class Logger:
@@ -14,7 +15,7 @@ class Logger:
 
     def _logger_configure(self):
         os.makedirs(self.directory, exist_ok=True)
-        file_name = datetime.now().strftime('%Y-%m-%d.txt')
+        file_name = datetime.now(timezone.utc).strftime('%Y-%m-%d.txt')
         path = os.path.join(self.directory, file_name)
 
         self.logger = logging.getLogger(self.name)
@@ -23,17 +24,18 @@ class Logger:
         if not self.logger.handlers:
             handler = logging.FileHandler(path, encoding='utf-8')
             formatter = logging.Formatter('%(asctime)s %(levelname)s [%(name)s]: %(message)s')
+            formatter.converter = time.gmtime
             handler.setFormatter(formatter)
             self.logger.addHandler(handler)
 
         self._remove_extra_logs()
 
     def _remove_extra_logs(self):
-        now = datetime.now()
+        now = datetime.now(timezone.utc)
         for file in os.listdir(self.directory):
             try:
                 date_str = file[:-4]
-                log_date = datetime.strptime(date_str, '%Y-%m-%d')
+                log_date = datetime.strptime(date_str, '%Y-%m-%d').replace(tzinfo=timezone.utc)
                 if (now - log_date).days >= self.days_of_interest:
                     os.remove(os.path.join(self.directory, file))
             except ValueError:
