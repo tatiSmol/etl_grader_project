d2bb90d37455c6e39ca654c66cac7b760c36714c
tatiSmol <judeclapton@icloud.com>
2025-04-15 15:32:11 +0300

    added automatic column width setting in GoogleSheetsClient class

--- google_sheets_client.py
+++ google_sheets_client.py
@@ -2,21 +2,21 @@ from datetime import datetime
 
 import gspread
 from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
 
 
 class GoogleSheetsClient:
     def __init__(self, credentials_path, spreadsheet_name):
         self.credentials_path = credentials_path
         self.spreadsheet_name = spreadsheet_name
-        self.client = self.__authorize()
 
-    def __authorize(self):
-        scopes = [
+        self.scopes = [
             'https://www.googleapis.com/auth/spreadsheets',
             'https://www.googleapis.com/auth/drive'
         ]
-        credentials = Credentials.from_service_account_file(self.credentials_path, scopes=scopes)
-        return gspread.authorize(credentials)
+        self.credentials = Credentials.from_service_account_file(self.credentials_path, scopes=self.scopes)
+        self.client = gspread.authorize(self.credentials)
+        self.service = build('sheets', 'v4', credentials=self.credentials)
 
     def upload_daily_summary(self, summary_data: dict):
         date_str = datetime.utcnow().strftime("%Y-%m-%d")
@@ -32,3 +32,19 @@ class GoogleSheetsClient:
         values = list(summary_data.values())
         worksheet.append_row(headers)
         worksheet.append_row(values)
+        self.__auto_resize_columns(sheet.id, worksheet._properties['sheetId'], len(summary_data))
+
+    def __auto_resize_columns(self, spreadsheet_id, sheet_id, col_count):
+        req_body = {'requests': [
+            {
+                'autoResizeDimensions': {
+                    'dimensions': {
+                        'sheetId': sheet_id,
+                        'dimension': 'COLUMNS',
+                        'startIndex': 0,
+                        'endIndex': col_count
+                    }
+                }
+            }
+        ]}
+        self.service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id, body=req_body).execute()
